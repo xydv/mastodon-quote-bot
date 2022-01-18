f740a95dee61a3584a95e1f109b236e553ac8965
icancodebeauty <adi@mail.io>
2022-01-18 06:54:59 +0000

    📜 Made Quote Bot!.

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Mastodon = require("mastodon-api");
+const fetch = require("cross-fetch");
+const ENV = require("dotenv");
+ENV.config();
+// const keepAlive = require('./server.js')
+// Quote Bot Starts
+console.log("Quote Bot Starting");
+// Mastodon Client
+const M = new Mastodon({
+  client_key: process.env.CLIENT_KEY,
+  client_secret: process.env.CLIENT_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  timeout_ms: 60 * 1000 * 5,
+  api_url: "https://mastodon.social/api/v1/",
+});
+// Fetch Quote Function
+const fetchQuote = async () => {
+  const apiURL = "https://api.quotable.io/random";
+  const fetchURL = await fetch(apiURL);
+  const dataJson = await fetchURL.json();
+  const posted = `"${dataJson.content + "\" | " + dataJson.author + "\n\n" + "#quote #motivation #quoteoftheday"}`;
+  const params = { status: posted };
+  // Mastodon Posts
+  M.post("statuses", params, (err) => { err ? console.log(err) : console.log("Posted Successfully!!.") });
+};
+// Call fetchQuote Function Every 4.5 Minutes
+setInterval(fetchQuote, 60000 * 4.5);
